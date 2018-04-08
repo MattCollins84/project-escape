5d9f86ad9109af51d97de67f1c2d5f1a1f902e65
Matt Collins <matt@strikesandgutters.com>
2018-04-08 14:51:09 +0100

    button

--- examples/blink.js
+++ examples/blink.js
@@ -1,10 +1,8 @@
 const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-const LED = new Gpio(17, 'out'); //use GPIO pin 4, and specify that it is output
+const LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
 const blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
-console.log(LED)
 function blinkLED() { //function to start blinking
-  console.log(LED.readSync() === 0);
   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
     LED.writeSync(1); //set pin state to 1 (turn LED on)
   } else {
