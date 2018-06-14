ab5b03fa3bda1a75487497a6185a8079bde9908b
Matt Collins <matt@strikesandgutters.com>
2018-06-14 16:38:59 +0100

    simple version

--- bunker/chess.js
+++ bunker/chess.js
@@ -1,38 +1,12 @@
 const GPIO = require('onoff').Gpio;
-const lock = new GPIO(4, 'out');    // Export GPIO4 as an output
+const lock = new GPIO(4, 'out');
+const magnet = new GPIO(17, 'in', 'both', { debounceTimeout: 10 });
 
-lock.writeSync(1)
-
-const circuits = [
-  { name: '1', set: false, pin: new GPIO(17, 'in', 'both', { debounceTimeout: 10 }) },
-  // { name: '2', set: false, pin: new GPIO(27, 'in', 'both', { debounceTimeout: 10 }) },
-  // { name: '3', set: false, pin: new GPIO(22, 'in', 'both', { debounceTimeout: 10 }) }
-]
-
-circuits.forEach(circuit => {
-  circuit.pin.watch((err, value) => {
-    if (err) return console.error('There was an error', err);
-    updateCircuit(circuit, value)
-  })
-});
-
-const updateCircuit = (circuit, value) => {
-  circuit.set = value === 1 ? true : false;
-  console.log(`${circuit.name}: ${circuit.set}`)
-  checkCircuits();
-}
-
-const checkCircuits = () => {
-  const allSet = circuits.every(c => c.set === true);
-  if (allSet) {
-    console.log("All circuits are set");
-    lock.writeSync(0)
-  } else {
-    const missing = circuits.filter(circuit => !circuit.set).map(circuit => circuit.name)
-    console.log(`Still missing ${missing.join(", ")}`)
-    lock.writeSync(1)
-  }
-}
+magnet.watch((err, value) => {
+  if (err) return console.error('There was an error', err);
+  console.log(value);
+  lock.writeSync(!value)
+})
 
 //function to run when exiting program
 function unexportOnClose() {
@@ -40,9 +14,6 @@ function unexportOnClose() {
     circuit.pin.unexport();
   });
 };
-
-
-checkCircuits()
 
 //function to run when user closes using ctrl+c
 process.on('SIGINT', unexportOnClose);
