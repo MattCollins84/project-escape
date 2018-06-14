552c7d1ca521164ad66f0f301285aa6b3787351b
Matt Collins <matt@strikesandgutters.com>
2018-06-14 16:54:03 +0100

    simple version

--- bunker/chess.js
+++ bunker/chess.js
@@ -2,11 +2,13 @@ const GPIO = require('onoff').Gpio;
 const lock = new GPIO(4, 'out');
 const magnet = new GPIO(17, 'in', 'both', { debounceTimeout: 10 });
 
-magnet.watch((err, value) => {
-  if (err) return console.error('There was an error', err);
-  console.log(value);
-  lock.writeSync(value ^ 1)
-})
+// magnet.watch((err, value) => {
+//   if (err) return console.error('There was an error', err);
+//   console.log(value);
+//   lock.writeSync(value ^ 1)
+// })
+
+lock.writeSync(0);
 
 //function to run when exiting program
 function unexportOnClose() {
