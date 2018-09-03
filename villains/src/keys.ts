7f75b0c1f4d1567ebe40e1df09c0a5ba1ff24ad5
Matt Collins <matt@strikesandgutters.com>
2018-09-03 17:38:17 +0100

    debounce

--- villains/src/keys.ts
+++ villains/src/keys.ts
@@ -7,23 +7,23 @@ const gamePort = argv.port ? argv.port : 5000
 
 const socket = io(`${gameHost}:${gamePort}`)
 // const redSwitch = new Switch(4, 'in', 'both', { debounceTimeout: 10 })
-const blueSwitch = new Switch(17, 'in', 'both', { debounceTimeout: 10 })
+const blueSwitch = new Switch(17, 'in', 'both', { debounceTimeout: 100 })
 
-// const compareValues = () => {
+const compareValues = () => {
 
-//   if (!redSwitch) return false;
-//   if (!blueSwitch) return false;
+  // if (!redSwitch.value) return false;
+  // if (!blueSwitch.value) return false;
 
-//   if (Math.abs(redSwitch.date - blueSwitch.date) <= 1000) {
-//     socket.emit('pauseTimer', { name: 'villains' })
-//     socket.emit('doSuccess', { name: 'villains' })
-//   }
+  if (blueSwitch.value) {
+    socket.emit('pauseTimer', { name: 'villains' })
+    socket.emit('doSuccess', { name: 'villains' })
+  }
 
-//   else {
-//     socket.emit('customEvent', { name: 'villains', event: 'denied' })
-//   }
+  else {
+    socket.emit('customEvent', { name: 'villains', event: 'denied' })
+  }
 
-// }
+}
 
 // redSwitch.on('value', compareValues)
 // blueSwitch.on('value', compareValues)
