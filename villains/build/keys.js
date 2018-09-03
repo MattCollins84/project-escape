f0cd13a51c8da815272a012f91a7f1a8f3c52d01
Matt Collins <matt@strikesandgutters.com>
2018-09-03 17:38:29 +0100

    debounce

--- villains/build/keys.js
+++ villains/build/keys.js
@@ -7,18 +7,18 @@ const gameHost = optimist_1.argv.host ? optimist_1.argv.host : 'http://localhost
 const gamePort = optimist_1.argv.port ? optimist_1.argv.port : 5000;
 const socket = io(`${gameHost}:${gamePort}`);
 // const redSwitch = new Switch(4, 'in', 'both', { debounceTimeout: 10 })
-const blueSwitch = new Switch_1.Switch(17, 'in', 'both', { debounceTimeout: 10 });
-// const compareValues = () => {
-//   if (!redSwitch) return false;
-//   if (!blueSwitch) return false;
-//   if (Math.abs(redSwitch.date - blueSwitch.date) <= 1000) {
-//     socket.emit('pauseTimer', { name: 'villains' })
-//     socket.emit('doSuccess', { name: 'villains' })
-//   }
-//   else {
-//     socket.emit('customEvent', { name: 'villains', event: 'denied' })
-//   }
-// }
+const blueSwitch = new Switch_1.Switch(17, 'in', 'both', { debounceTimeout: 100 });
+const compareValues = () => {
+    // if (!redSwitch.value) return false;
+    // if (!blueSwitch.value) return false;
+    if (blueSwitch.value) {
+        socket.emit('pauseTimer', { name: 'villains' });
+        socket.emit('doSuccess', { name: 'villains' });
+    }
+    else {
+        socket.emit('customEvent', { name: 'villains', event: 'denied' });
+    }
+};
 // redSwitch.on('value', compareValues)
 // blueSwitch.on('value', compareValues)
 console.log('hi');
