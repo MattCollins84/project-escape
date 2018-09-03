84dc22a63eb2c4677bf252e0c68f54cbf556ec59
Matt Collins <matt@strikesandgutters.com>
2018-09-03 17:32:16 +0100

    debug

--- villains/build/keys.js
+++ villains/build/keys.js
@@ -6,22 +6,20 @@ const io = require("socket.io-client");
 const gameHost = optimist_1.argv.host ? optimist_1.argv.host : 'http://localhost';
 const gamePort = optimist_1.argv.port ? optimist_1.argv.port : 5000;
 const socket = io(`${gameHost}:${gamePort}`);
-const redSwitch = new Switch_1.Switch(4, 'in', 'both', { debounceTimeout: 10 });
+// const redSwitch = new Switch(4, 'in', 'both', { debounceTimeout: 10 })
 const blueSwitch = new Switch_1.Switch(17, 'in', 'both', { debounceTimeout: 10 });
-const compareValues = () => {
-    if (!redSwitch)
-        return false;
-    if (!blueSwitch)
-        return false;
-    if (Math.abs(redSwitch.date - blueSwitch.date) <= 1000) {
-        socket.emit('pauseTimer', { name: 'villains' });
-        socket.emit('doSuccess', { name: 'villains' });
-    }
-    else {
-        socket.emit('customEvent', { name: 'villains', event: 'denied' });
-    }
-};
-redSwitch.on('value', compareValues);
-blueSwitch.on('value', compareValues);
+// const compareValues = () => {
+//   if (!redSwitch) return false;
+//   if (!blueSwitch) return false;
+//   if (Math.abs(redSwitch.date - blueSwitch.date) <= 1000) {
+//     socket.emit('pauseTimer', { name: 'villains' })
+//     socket.emit('doSuccess', { name: 'villains' })
+//   }
+//   else {
+//     socket.emit('customEvent', { name: 'villains', event: 'denied' })
+//   }
+// }
+// redSwitch.on('value', compareValues)
+// blueSwitch.on('value', compareValues)
 console.log('hi');
 //# sourceMappingURL=keys.js.map
--- villains/src/keys.ts
+++ villains/src/keys.ts
@@ -6,26 +6,26 @@ const gameHost = argv.host ? argv.host : 'http://localhost'
 const gamePort = argv.port ? argv.port : 5000
 
 const socket = io(`${gameHost}:${gamePort}`)
-const redSwitch = new Switch(4, 'in', 'both', { debounceTimeout: 10 })
+// const redSwitch = new Switch(4, 'in', 'both', { debounceTimeout: 10 })
 const blueSwitch = new Switch(17, 'in', 'both', { debounceTimeout: 10 })
 
-const compareValues = () => {
+// const compareValues = () => {
 
-  if (!redSwitch) return false;
-  if (!blueSwitch) return false;
+//   if (!redSwitch) return false;
+//   if (!blueSwitch) return false;
 
-  if (Math.abs(redSwitch.date - blueSwitch.date) <= 1000) {
-    socket.emit('pauseTimer', { name: 'villains' })
-    socket.emit('doSuccess', { name: 'villains' })
-  }
+//   if (Math.abs(redSwitch.date - blueSwitch.date) <= 1000) {
+//     socket.emit('pauseTimer', { name: 'villains' })
+//     socket.emit('doSuccess', { name: 'villains' })
+//   }
 
-  else {
-    socket.emit('customEvent', { name: 'villains', event: 'denied' })
-  }
+//   else {
+//     socket.emit('customEvent', { name: 'villains', event: 'denied' })
+//   }
 
-}
+// }
 
-redSwitch.on('value', compareValues)
-blueSwitch.on('value', compareValues)
+// redSwitch.on('value', compareValues)
+// blueSwitch.on('value', compareValues)
 
 console.log('hi')
