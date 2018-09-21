7a0a74bc0619e99048f11c043b7f9ca582c36486
Matt Collins <matt@strikesandgutters.com>
2018-09-21 16:30:57 +0100

    remove three

--- villains/src/order.ts
+++ villains/src/order.ts
@@ -2,7 +2,7 @@ import { Switch } from './lib/Switch';
 
 const one = new Switch(17, 'in', 'rising', { debounceTimeout: 100 })
 const two = new Switch(27, 'in', 'rising', { debounceTimeout: 100 })
-const three = new Switch(22, 'in', 'rising', { debounceTimeout: 10 })
+// const three = new Switch(22, 'in', 'rising', { debounceTimeout: 10 })
 const four = new Switch(26, 'in', 'rising', { debounceTimeout: 10 })
 // const five = new Switch(17, 'in', 'rising', { debounceTimeout: 10 })
 // const six = new Switch(17, 'in', 'rising', { debounceTimeout: 10 })
