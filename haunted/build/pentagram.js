7367fd5f71c57279d000f3dfde5e202c3ff325ee
Matt Collins <matt@strikesandgutters.com>
2019-09-02 19:25:45 +0100

    socket io

--- haunted/build/pentagram.js
+++ haunted/build/pentagram.js
@@ -1,9 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Switch_1 = require("./lib/Switch");
+const SocketIO = require("socket.io");
+const optimist_1 = require("optimist");
+const port = optimist_1.argv.port ? optimist_1.argv.port : 5000;
+const io = SocketIO(port);
 const debounce = 200;
 const magnets = new Switch_1.Switch(17, 'in', 'both', { debounceTimeout: debounce });
+const config = {
+    activated: false
+};
+io.on('connect', id => {
+    console.log('client connected', id);
+});
 magnets.on('value', () => {
     console.log(magnets.value);
+    if (magnets.value && config.activated === false) {
+        io.emit('play-video');
+        config.activated = true;
+        console.log('Triggering video');
+    }
 });
 //# sourceMappingURL=pentagram.js.map
---
+++ haunted/build/video-client.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const io = require("socket.io-client");
+const optimist_1 = require("optimist");
+const host = optimist_1.argv.host ? optimist_1.argv.host : 'http://localhost';
+const port = optimist_1.argv.port ? optimist_1.argv.port : 5000;
+const socket = io(`${host}:${port}`);
+socket.on('connect', () => {
+    console.log('connected to server');
+});
+socket.on('play-video', data => {
+    console.log('playing video', data);
+});
+//# sourceMappingURL=video-client.js.map
