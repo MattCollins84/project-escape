c89672c03f58b3a6a08cd55c12a46a42ece37b2d
Matt Collins <matt@strikesandgutters.com>
2019-09-02 19:24:13 +0100

    socket io

--- haunted/src/pentagram.ts
+++ haunted/src/pentagram.ts
@@ -1,8 +1,28 @@
 import { Switch } from './lib/Switch';
+import * as SocketIO from 'socket.io';
+import { argv } from 'optimist'
+
+const port = argv.port ? argv.port : 5000
+
+const io = SocketIO(port)
 
 const debounce = 200;
 const magnets = new Switch(17, 'in', 'both', { debounceTimeout: debounce });
 
+const config = {
+  activated: false
+}
+
+io.on('connect', id => {
+  console.log('client connected', id)
+})
+
 magnets.on('value', () => {
   console.log(magnets.value)
+
+  if (magnets.value && config.activated === false) {
+    io.emit('play-video');
+    config.activated = true;
+    console.log('Triggering video')
+  }
 })
---
+++ haunted/src/video-client.ts
@@ -0,0 +1,15 @@
+import * as io from 'socket.io-client';
+import { argv } from 'optimist'
+
+const host = argv.host ? argv.host : 'http://localhost'
+const port = argv.port ? argv.port : 5000
+
+const socket = io(`${host}:${port}`)
+
+socket.on('connect', () => {
+  console.log('connected to server')
+})
+
+socket.on('play-video', data => {
+  console.log('playing video', data)
+})
